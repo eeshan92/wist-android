f3ac5d4716ac79e30f4bf36ef960f44ccf9a712e
Ee Shan <eeshansim@gmail.com>
2017-06-15 23:40:17 +0800

    update splash screen

--- app/src/main/java/com/example/eeshan/wist/SplashActivity.java
+++ app/src/main/java/com/example/eeshan/wist/SplashActivity.java
@@ -14,6 +14,12 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
